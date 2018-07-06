88308f1d4324da66f7dcb69c0da9ed48046d581b
pwirth <wirth@hallowelt.biz>
2018-07-06 10:15:30 +0200

    Added special page to list all topics

---
+++ languages/Topics.alias.php
@@ -0,0 +1,12 @@
+<?php
+$specialPageAliases = array();
+
+/** English */
+$specialPageAliases['en'] = array(
+	'Topics' => array( 'Topics' ),
+);
+
+/** German (Deutsch) */
+$specialPageAliases['de'] = array(
+	'Topics' => array( 'Topics', 'Themen' ),
+);
